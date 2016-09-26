51cbc9be9915ac4da4fe02114ba3b3a159b9cae7
Abuenavista <Abuenavi@gmail.com>
2016-09-26 13:09:37 -0700

    Functional test moved to own directory

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+#import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
+#class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -20,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -65,6 +67,3 @@ class NewVisitorTest(unittest.TestCase):
     	# that the site has generated a unique URL for her -- there is some
     	# explanatory text to that effect.
 		self.fail('Finish the test!') 
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
