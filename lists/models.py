2dabaf1ac914693c3698a684d754eb0269dcd3c0
Abuenavista <Abuenavi@gmail.com>
2016-09-26 09:05:54 -0700

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):		#I am inheriting from models.Model class
+	#pass
+	text = models.TextField(default='')	#means we will have a model in db that is a TextField with a default value
